5c2df69efa37ead9b8c76e3d5d2ca0f61cd9b784
Sanchez Fernandes <sanchezfernandesmusic@gmail.com>
2024-06-05 10:36:29 -0400

    added hero image

--- portfolio/src/Components/Hero/Hero.jsx
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import "./Hero.scss";
+import sanchez from "../../assets/file.png";
 
 const Hero = () => {
     return (
         <main className='heroMain'>
-            <section>
+            <section className='titleDiv'>
                 <h2 className='heroTitle'>Hello I’m Sanchez , I am a full stack software engineer based in New York City. </h2>
+            </section>
+            <section className='pictureDiv'>
+                <img className='sanchez' src={sanchez} alt="sanchez" />
             </section>
         </main>
     )
