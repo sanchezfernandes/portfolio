7a47f42da7e49966269928e801b8e8526f613ad0
Sanchez Fernandes <sanchezfernandesmusic@gmail.com>
2024-06-06 11:27:58 -0400

    creates and sizes hero banner

--- portfolio/src/Components/Hero/Hero.jsx
+++ portfolio/src/Components/Hero/Hero.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import "./Hero.scss";
 import sanchez from "../../assets/file.png";
+import hero from "../../assets/hero.png";
 
 const Hero = () => {
     return (
         <main className='heroMain'>
-            <section className='titleDiv'>
+            <img className='heroImg' src={hero} alt="hero image" />
+
+            {/* <section className='titleDiv'>
                 <h2 className='heroTitle'>Hello I’m Sanchez , I am a full stack software engineer based in New York City. </h2>
             </section>
             <section className='pictureDiv'>
                 <img className='sanchez' src={sanchez} alt="sanchez" />
-            </section>
+            </section> */}
         </main>
     )
 }
